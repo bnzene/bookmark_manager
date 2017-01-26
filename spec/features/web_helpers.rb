fe0cd723d127f0fabb4238d7755b89eb21094fa6
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-01-26 14:58:10 +0000

    working tag feature, refactor tests and datamapper

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -2,13 +2,10 @@ def create_link
   visit '/links/new'
   fill_in :title, :with => 'MySpace'
   fill_in :URL, :with => 'myspace.com'
+  yield if block_given?
   click_button 'Submit'
 end
 
 def create_tagged_link
-  visit '/links/new'
-  fill_in :title, :with => 'MySpace'
-  fill_in :URL, :with => 'myspace.com'
-  fill_in :tags, :with => 'music'
-  click_button 'Submit'
+  create_link{fill_in :tags, :with => 'music'}
 end
